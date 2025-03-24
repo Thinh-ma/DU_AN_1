ac4132c6dd25b0fd0a3d1737a556f7eace696cf2
Thinh-ma <ngpthinh50@gmail.com>
2025-03-24 20:25:11 +0700

    Client [Header]

--- App/Controllers/Client/ProductController.php
+++ App/Controllers/Client/ProductController.php
@@ -4,99 +4,49 @@ namespace App\Controllers\Client;
 
 use App\Helpers\AuthHelper;
 use App\Helpers\NotificationHelper;
-use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Product;
 use App\Views\Client\Components\Notification;
 use App\Views\Client\Layouts\Footer;
 use App\Views\Client\Layouts\Header;
-use App\Views\Client\Pages\Product\Category as ProductCategory;
+use App\Views\Client\Pages\Product\Category;
 use App\Views\Client\Pages\Product\Detail;
 use App\Views\Client\Pages\Product\Index;
+use App\Views\Client\Pages\Product\Checkout;
 
 class ProductController
 {
     // hiển thị danh sách
     public static function index()
     {
-        // giả sử data là mảng dữ liệu lấy được từ database
-        $categories = [
-            [
-                'id' => 1,
-                'name' => 'Category 1',
-                'status' => 1
-            ],
-            [
-                'id' => 2,
-                'name' => 'Category 2',
-                'status' => 1
-            ],
-            [
-                'id' => 3,
-                'name' => 'Category 3',
-                'status' => 0
-            ],
-
-        ];
-        $products = [
-            [
-                'id' => 1,
-                'name' => 'Product 1',
-                'description' => 'Description Product 1',
-                'price' => 100000,
-                'discount_price' => 10000,
-                'image' => 'product.jpg',
-                'status' => 1
-            ],
-            [
-                'id' => 2,
-                'name' => 'Product 2',
-                'description' => 'Description Product 2',
-                'price' => 200000,
-                'discount_price' => 20000,
-                'image' => 'product.jpg',
-                'status' => 1
-            ],
-            [
-                'id' => 3,
-                'name' => 'Product 3',
-                'description' => 'Description Product 3',
-                'price' => 300000,
-                'discount_price' => 30000,
-                'image' => 'product.jpg',
-                'status' => 1
-            ],
-
-        ];
-        $data = [
-            'products' => $products,
-            'categories' => $categories
-        ];
+        
         Header::render();
 
-        Index::render($data);
+        Index::render();
         Footer::render();
     }
-    public static function detail($id)
+    public static function detail()
     {
-        $product_detail = [
-            'id' => $id,
-            'name' => 'Product 1',
-            'description' => 'Description Product 1',
-            'price' => 100000,
-            'discount_price' => 10000,
-            'image' => 'product.jpg',
-            'status' => 1
-        ];
-        $data = [
-            'product' => $product_detail
-        ];
+       
         Header::render();
 
-        Detail::render($data);
+        Detail::render();
         Footer::render();
     }
-    public static function getProductByCategory($id)
-    {
+
+
+    public static function category() {
+        Header::render();
+
+        category::render();
+        Footer::render();
     }
+
+    public static function checkout() {
+        Header::render();
+
+        Checkout::render();
+        Footer::render();
+    }
+    
 }
--- index.php
+++ index.php
@@ -20,7 +20,16 @@ require_once 'config.php';
 // *** Client
 Route::get('/', 'App\Controllers\Client\HomeController@index');
 Route::get('/products', 'App\Controllers\Client\ProductController@index');
-Route::get('/products/{id}', 'App\Controllers\Client\ProductController@detail');
+Route::get('/productsDeatail', 'App\Controllers\Client\ProductController@detail');
+Route::get('/productsCategory', 'App\Controllers\Client\ProductController@category');
+Route::get('/productsCheckout', 'App\Controllers\Client\ProductController@checkout');
+
+// trang giỏ hàng
+Route::get('/cart', 'App\Controllers\Client\CartController@index');
+
+
+//trang bài viết
+Route::get('/post', 'App\Controllers\Client\PostController@index');
 
 
 // *** Admin
